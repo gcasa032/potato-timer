5fabfac98635535e046d1788bef51d662effd14d
Guillermo Casal <gcasa032@fiu.edu>
2021-07-23 15:30:46 -0400

    login/register forms are now controlled components

--- src/components/login-register-view/login.js
+++ src/components/login-register-view/login.js
@@ -1,27 +1,65 @@
+import React from "react";
+
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function Login(props){
-    return (
-        <Card.Body className="text-center">
-            <Card.Title as="h3">
-                Login
-            </Card.Title>
-            <hr/>
-            <Form className="d-grid gap-2">
-                <Form.Group className="mb-2" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
-                <Form.Group className="mb-2" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button className="mb-3" variant="primary" type="submit">
+class Login extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name] : value
+        });
+    }
+
+    render(){
+        return (
+            <Card.Body className="text-center">
+                <Card.Title as="h3">
                     Login
-                </Button>
-            </Form>
-        </Card.Body>
-    );
+                </Card.Title>
+                <hr/>
+                <Form className="d-grid gap-2">
+                    <Form.Group className="mb-2" controlId="formBasicEmail">
+                        <Form.Control 
+                            type="email"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            placeholder="Enter email" 
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-2" controlId="formBasicPassword">
+                        <Form.Control 
+                            type="password"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            placeholder="Password" 
+                        />
+                    </Form.Group>
+                    <Button className="mb-3" variant="primary" type="submit">
+                        Login
+                    </Button>
+                </Form>
+            </Card.Body>
+        );
+    }
 }
 
 export default Login;
--- src/components/login-register-view/register.js
+++ src/components/login-register-view/register.js
@@ -1,10 +1,38 @@
+import React from 'react';
+
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 
-function Register(props){
-    return (
+class Register extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            email: "",
+            password: "",
+            confPassword: "",
+        };
+
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name] : value
+        });
+    }
+
+    render(){
+
+        return (
         <Card.Body className="text-center">
             <Card.Title as="h3">
                 Register
@@ -12,23 +40,51 @@ function Register(props){
             <hr/>
             <Form className="d-grid gap-2">
                 <Form.Group className="mb-2" controlId="formUsername">
-                    <Form.Control type="text" placeholder="Username" />
+                    <Form.Control 
+                        type="text"
+                        value={this.state.username}
+                        name="username" 
+                        placeholder="Username"
+                        onChange={this.handleChange}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-2" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Email" />
+                    <Form.Control 
+                        type="email"
+                        value={this.state.email} 
+                        name="email" 
+                        placeholder="Email"
+                        onChange={this.handleChange}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-2" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control 
+                        type="password"
+                        value={this.state.password}
+                        name="password" 
+                        placeholder="Password"
+                        onChange={this.handleChange}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-2" controlId="formConfirmPassword">
-                    <Form.Control type="password" placeholder="Confirm Password" />
+                    <Form.Control 
+                        type="password"
+                        value={this.state.confPassword}
+                        name="confPassword" 
+                        placeholder="Confirm Password"
+                        onChange={this.handleChange} 
+                    />
                 </Form.Group>
                 <Button className="mb-3" variant="primary" type="submit">
                     Register
                 </Button>
             </Form>
+            
         </Card.Body>
     );
+
+    }
+
 }
 
 export default Register;
